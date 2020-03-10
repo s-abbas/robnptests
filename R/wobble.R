7a6c4e24719fdb763dc934bb3ca8a7c058560f9b
b-brune <barbara.brune@tu-dortmund.de>
2020-03-10 17:03:32 +0100

    updated wobble function

--- R/wobble.R
+++ R/wobble.R
@@ -8,18 +8,33 @@ wobble <- function(x, y) {
   no.values.y <- length(unique(y))
   no.values.xy <- length(unique(c(x, y)))
 
+
   if (no.values.x == length(x) & no.values.y == length(y) & no.values.xy == length(c(x, y))) {
     ## If all values are distinct, return original observations
     return(list(x = x, y = y))
   } else {
     z <- c(x, y)
 
-    ## Maximal number of digits after decimal point
-    max.digits <- max(sapply(strsplit(as.character(z), "\\."), length)) - 1
+    ## Maximal number of digits after decimal point:
+    # this expression always returns two (part before and part after the decimal
+    # point)
+    # max.digits <- max(sapply(strsplit(as.character(z), "\\."), length)) - 1
+    # Instead:
+    digits <- sapply(as.character(z),
+                     function(x) nchar(unlist(strsplit(x, "\\."))[2]))
+    digits[is.na(digits)] <- 0 # if the values are discrete we will get NA from the strsplit
 
+    # I propose we use the full range of values between the observations:
     ## Add random noise from a uniform distribution
-    z.wobble <- z + runif(length(z), min = -10^(-(max.digits + 1)), max = 10^(-(max.digits + 1)))
+    z.wobble <- z + runif(length(z),
+                          -0.5*10^(-max(digits)), 0.5*10^(-max(digits)))
+
+    #z.wobble <- z + runif(length(z), min = -10^(-(max.digits + 1)), max = 10^(-(max.digits + 1)))
 
     return(list(x = z.wobble[1:length(x)], y = z.wobble[(length(x) + 1):length(y)]))
   }
 }
+
+
+
+
