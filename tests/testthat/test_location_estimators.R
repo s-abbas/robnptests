601bc52f972e51f227dc1310cabc762c17b749b0
s-abbas <sermad.abbas@tu-dortmund.de>
2020-04-13 17:11:19 +0200

    Corrected test cases.

--- tests/testthat/test_location_estimators.R
+++ tests/testthat/test_location_estimators.R
@@ -6,25 +6,25 @@ testthat::test_that("hodges_lehmann works correctly", {
   ## Computation of one-sample Hodges-Lehmann estimator
   ## ___________________________________________________________________________
   x <- c(1, 3, 8, 9, 15)
-  expect_equal(hodges_lehmann(x), 7)
+  testthat::expect_equal(hodges_lehmann(x), 7)
 
   ## ___________________________________________________________________________
   ## Handling of missing values
   ## ___________________________________________________________________________
   y <- c(x, NA)
-  expect_equal(hodges_lehmann(y, na.rm = TRUE), 7)
-  expect_true(is.na(hodges_lehmann(y, na.rm = FALSE)))
+  testthat::expect_equal(hodges_lehmann(y, na.rm = TRUE), 7)
+  testthat::expect_true(is.na(hodges_lehmann(y, na.rm = FALSE)))
 
   ## ___________________________________________________________________________
   ## Non-numeric input
   ## ___________________________________________________________________________
   z <- letters
-  expect_error(hodges_lehmann(z))
+  testthat::expect_error(hodges_lehmann(z))
 }
 )
 
 ## Two-sample Hodges-Lehmann estimator
-testthat::test_that("hodges_lehmann works correctly", {
+testthat::test_that("hodges_lehmann_2sample works correctly", {
   ## ___________________________________________________________________________
   ## Computation of two-sample Hodges-Lehmann estimator
   ## ___________________________________________________________________________
@@ -40,7 +40,7 @@ testthat::test_that("hodges_lehmann works correctly", {
 
   ## Two-sample Wilcoxon rank-sum statistic of aligned sampled needs to be
   ## equal to its expected value under H_0 of two-sample Wilcoxon rank-sum test
-  expect_equal(sum(rank(c(x, y))[1:m]), m * (m + n + 1)/2)
+  testthat::expect_equal(sum(rank(c(x, y))[1:m]), m * (m + n + 1)/2)
 
   ## ___________________________________________________________________________
   ## Handling of missing values
@@ -54,14 +54,14 @@ testthat::test_that("hodges_lehmann works correctly", {
   n <- length(y)
 
   ## Align second sample after removing missing values
-  y <- y - hodges_lehmann_2sample(x, y, na.rm = TRUE)
+  x <- x - hodges_lehmann_2sample(x, y, na.rm = TRUE)
 
-  ## Two-sample Wilcoxon rank-sum statistic of aligned sampled needs to be
+  ## Two-sample Wilcoxon rank-sum statistic of aligned samples needs to be
   ## equal to its expected value under H_0 of two-sample Wilcoxon rank-sum test
-  expect_equal(sum(rank(c(x, y))[1:m]), m * (m + (n - 1) + 1)/2)
+  testthat::expect_equal(sum(rank(c(x, y))[1:m]), m * (m + (n - 1) + 1)/2)
 
   ## If missing values are not removed, the function should return NA
-  expect_true(is.na(hodges_lehmann_2sample(x, y, na.rm = FALSE)))
+  testthat::expect_true(is.na(hodges_lehmann_2sample(x, y, na.rm = FALSE)))
 
   ## ___________________________________________________________________________
   ## Non-numeric input
@@ -72,6 +72,6 @@ testthat::test_that("hodges_lehmann works correctly", {
   y <- letters[1:12]
 
   ## If there are non-numeric input values, the function should throw a warning
-  expect_warning(hodges_lehmann_2sample(x, y))
+  testthat::expect_warning(hodges_lehmann_2sample(x, y))
 }
 )
