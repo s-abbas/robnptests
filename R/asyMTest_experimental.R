fec99915fa0c3a5808f3164447b03b492aaa60b7
b-brune <barbara.brune@tu-dortmund.de>
2020-08-24 10:06:05 +0200

    included a different sigma0 in scaleTau2

--- R/asyMTest_experimental.R
+++ R/asyMTest_experimental.R
@@ -9,8 +9,8 @@ m_test2 <- function(x, y, alternative = c("two.sided", "greater", "less")) {
   ## Compute test statistic
   ## ___________________________________________________________________________
   ## M-estimates
-  est.x <- huberM(x, k = 1.5)
-  est.y <- huberM(y, k = 1.5)
+  est.x <- robustbase::huberM(x, k = 1.5)
+  est.y <- robustbase::huberM(y, k = 1.5)
 
   ## Estimator for \nu
   psi.x <- robustbase::Mpsi((x - est.x$mu)/est.x$s, psi = "huber", cc = 1.5)
@@ -24,7 +24,9 @@ m_test2 <- function(x, y, alternative = c("two.sided", "greater", "less")) {
   nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
 
   #res[i] <- sqrt((m * n)/(m + n)) * (est.x$mu - est.y$mu)/((nu.x + nu.y)/2)
-  statistic <- (est.x$mu - est.y$mu)/sqrt((n * scaleTau2(x, consistency = FALSE)^2 * nu.x + m * scaleTau2(y, consistency = FALSE)^2 * nu.y)/(m * n))
+  statistic <- (est.x$mu - est.y$mu) /
+    sqrt((n * robustbase::scaleTau2(x, consistency = FALSE)^2 * nu.x +
+            m * robustbase::scaleTau2(y, consistency = FALSE)^2 * nu.y) / (m * n))
 
   ## ___________________________________________________________________________
   ## Test decision
@@ -52,12 +54,12 @@ for (i in 1:1000) {
 
 
 
-res <- numeric(1000)
+res <- numeric(10000)
 
 m <- n <- 100
 
 
-for (i in 1:1000) {
+for (i in 1:10000) {
   x <- rt(m, df = 3)
   y <- rt(n, df = 3)
 
@@ -77,12 +79,13 @@ for (i in 1:1000) {
   nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
 
   #res[i] <- sqrt((m * n)/(m + n)) * (est.x$mu - est.y$mu)/((nu.x + nu.y)/2)
-  res[i] <- (est.x$mu - est.y$mu)/sqrt((n * scaleTau2(x)^2 * nu.x + m * scaleTau2(y)^2 * nu.y)/(m * n))
+  res[i] <- (est.x$mu - est.y$mu)/sqrt((n * scaleTau2(x, sigma0=mad(x))^2 * nu.x + m * scaleTau2(y, sigma0 = mad(y))^2 * nu.y)/(m * n))
 }
 
 
-hist(res, freq = FALSE, xlim = c(-5, 5), ylim = c(0, 0.4))
+hist(res, freq = FALSE, xlim = c(-5, 5), ylim = c(0, 0.4), breaks = 100)
 curve(dnorm(x, sd = 1), from = -5, to = 5, add = TRUE)
+
 
 qqnorm(res)
 
