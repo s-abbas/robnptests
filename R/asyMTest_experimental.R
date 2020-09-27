6f3831c64c7c2101853922fcdb108a9212704e6f
s-abbas <sermad.abbas@tu-dortmund.de>
2020-09-27 21:36:22 +0200

    Added documentation to new m_test function.

--- R/asyMTest_experimental.R
+++ R/asyMTest_experimental.R
@@ -2,11 +2,10 @@
 ## Two-sample location test based on difference of M-estimators
 ## ----------------------------------------------------------------------------
 
-#' Two-sample location tests based on difference of M-estimators
+#' @title Two sample location test based on M-estimators
 #'
-#' @description
-#' \code{m_test} performs a two-sample location test based on
-#' the difference of M-estimates of both samples.
+#' @description \code{m_estimator_test} performs a two-sample location test
+#'              based on M-estimators.
 #'
 #' @template x
 #' @template y
@@ -21,22 +20,94 @@
 #' @template wobble
 #' @template seed
 #'
+#' @details
+#' The test statistic is the difference of M-estimates for both samples
+#' divided by a pooled tau-estimate for the within-sample variance.
+#'
+#' When computing a randomization distribution based on randomly drawn splits
+#' with replacement, the results of Smyth & Phipson (2010) to calculate the p-value
+#' are used. The psi and rho functions, which are needed to obtain the M-estimates,
+#' are computed via the implementations in the package \code{\link[robustbase]{robustbase}}.
+#' The tau scale estimate is computed with the default parameter settings
+#' of the function \code{\link[robustbase]{scaleTau2}}.
+#'
+#' The test statistic is given by
+#'
+#' TODO: Include test statistic
+#'
+#' which follows asymptotically a standard normal distribution. However, this is
+#' only guaranteed for normally distributed data. In case of a non-normal
+#' distribution, the test might not keep the desired significance level. This
+#' can be corrected by correcting the test statistic by a factor which has to
+#' be determined individually for each distribution.
+#'
+#' For \code{var.test = TRUE}, the test compares the two samples for a difference in scale.
+#' This is achieved by log-transforming the original observations so that a potential
+#' scale difference appears as a location difference between the transformed samples;
+#' see Fried (2012). The sample cannot contain zeros due to the necessary log-transformation.
+#' If it contains zeros, uniform noise is added to all variables in order to remove zeros.
+#' A warning is printed.
+#'
+#' If the sample has been modified (either because of 0's for \code{var.test = TRUE}, or
+#' as \code{wobble = TRUE}, the modified samples can be retrieved using
+#'
+#' \code{set.seed(wobble.seed); wobble(x, y)}
+#'
+#' @return
+#' A list with class "\code{htest}" containing the following components:
+#' \item{statistic}{the value of the test statistic.}
+#' \item{parameter}{the degrees of freedom for the test statistic.}
+#' \item{p.value}{the p-value for the test.}
+#' \item{estimate}{the Huber M-estimates of \code{x} and \code{y}.}
+#' \item{null.value}{the specified hypothesized value of the mean difference.}
+#' \item{alternative}{a character string describing the alternative hypothesis.}
+#' \item{method}{a character string indicating what type of test was performed.}
+#' \item{data.name}{a character string giving the names of the data.}
+#'
 #' @export
 
 m_test <- function(x, y, alternative = c("two.sided", "greater", "less"),
                    delta = ifelse(var.test, 1, 0),
                    method = c("asymptotic", "permutation", "randomization"),
                    psi = c("huber", "hampel", "bisquare"),
-                   k = .Mpsi.tuning.default(psi),
+                   k = robustbase::.Mpsi.tuning.default(psi),
                    n.rep = 10000, na.rm = FALSE,
                    var.test = FALSE, wobble = FALSE, wobble.seed = NULL) {
 
+  ## ___________________________________________________________________________
+  ## Error messages
+  ## ___________________________________________________________________________
+
+  ## alternative
+  if (!all((alternative %in% c("two.sided", "greater", "less")))) {
+    stop (" 'alternative' must be one of 'two.sided', 'greater' or 'less'. ")
+  }
+
+  ## delta
+  if (!missing(delta) && (length(delta) != 1 || is.na(delta))) {
+    stop ("'delta' must be a single number.")
+  }
+
+  ## method
+  if (!all((method %in% c("asymptotic", "permutation", "randomization")))) {
+    stop (" 'method' must be one of 'asymptotic', 'permutation' or 'randomization'. ")
+  }
+
+  ## psi
+  if (!all((psi %in% c("huber", "hampel", "bisquare")))) {
+    stop (" 'psi' must be one of 'huber', 'hampel' or 'bisquare'. ")
+  }
+
+  ## ___________________________________________________________________________
+  ## Extract default argument values
+  ## ___________________________________________________________________________
+
   alternative <- match.arg(alternative)
-  #method <- match.arg(method)
   psi <- match.arg(psi)
 
-  ## Names of data sets
-  dname <- paste(deparse(substitute(x)), "and", deparse(substitute(y)))
+  ## ___________________________________________________________________________
+  ## Check and modify samples
+  ## ___________________________________________________________________________
 
   ## NA handling
   if (!na.rm & (any(is.na(x)) | any(is.na(y)))) {
@@ -69,7 +140,9 @@ m_test <- function(x, y, alternative = c("two.sided", "greater", "less"),
   if (var.test) {
     if (any(c(x, y) == 0)) {
 
-      if (is.null(wobble.seed)) wobble.seed <- sample(1e6, 1)
+      if (is.null(wobble.seed)) {
+        wobble.seed <- sample(1e6, 1)
+      }
       set.seed(wobble.seed)
 
       xy <- wobble(x, y, check = FALSE)
@@ -84,17 +157,11 @@ m_test <- function(x, y, alternative = c("two.sided", "greater", "less"),
     delta <- log(delta^2)
   }
 
-  ## Error handling
-  if (!missing(delta) && (length(delta) != 1 || is.na(delta))) {
-    stop ("'delta' must be a single number.")
-  }
+  ## ___________________________________________________________________________
+  ## Set method for computation of p-value if not specified by user
+  ## ___________________________________________________________________________
 
-  if (!all((method %in% c("asymptotic", "permutation", "randomization")))) {
-    stop (" 'method' must be one of 'asymptotic', 'permutation' or 'randomization'. ")
-  }
-
-  ## If no choice is made regarding the computation of the p-value, the method
-  ## is automatically selected based on the sample sizes
+  ## The method is automatically selected based on the sample sizes
   if (length(method) > 1 & identical(method, c("asymptotic", "permutation", "randomization"))) {
     if (length(x) >= 30 & length(y) >= 30) {
       method <- "asymptotic"
@@ -105,95 +172,64 @@ m_test <- function(x, y, alternative = c("two.sided", "greater", "less"),
     }
   }
 
+  ## ___________________________________________________________________________
+  ## Test decision
+  ## ___________________________________________________________________________
+
+  ## Test statistic and location estimates for both samples
+  stats <- m_test_statistic(x, y - delta, psi = psi, k = k)
+  statistic <- stats$statistic
+  estimates <- stats$estimates
+
   if (method %in% c("permutation", "randomization")) {
     ## _________________________________________________________________________
-    ## Compute test statistic
+    ## Test decision for permutation and randomization test
     ## _________________________________________________________________________
-    stats <- m_test_statistic(x, y - delta, psi = psi, k = k)
-    statistic <- stats$statistic
 
-    estimates <- stats$estimates
-
+    ## Permutation or randomization distribution
     distribution <- mest_perm_distribution(x = x, y = y - delta, randomization = (method == "randomization"),
                                            n.rep = n.rep, psi = psi, k = k)
 
+    ## p-value
     p.value <- calc_perm_p_value(statistic, distribution, m = length(x), n = length(y),
-                                 randomization = randomization, n.rep = n.rep, alternative = alternative)
+                                 randomization = (method == "randomization"), n.rep = n.rep, alternative = alternative)
   } else if (method == "asymptotic") {
-    m <- length(x)
-    n <- length(y)
-
     ## _________________________________________________________________________
-    ## Compute test statistic
+    ## Test decision for asymptotic test
     ## _________________________________________________________________________
 
-    ## M-estimates for both samples
-    est.x <- m_est(x = x, psi = psi, k = k)$est
-    est.y <- m_est(x = y, psi = psi, k = k)$est
-
-    ## Estimator for \nu
-    psi.x <- robustbase::Mpsi((x - est.x)/mad(x), psi = psi, cc = k)
-    rho.x <- robustbase::Mpsi((x - est.x)/mad(x), psi = psi, cc = k, deriv = 1)
-
-    psi.y <- robustbase::Mpsi((y - est.y)/mad(y), psi = psi, cc = k)
-    rho.y <- robustbase::Mpsi((y - est.y)/mad(y), psi = psi, cc = k, deriv = 1)
-
-    nu.x <- mean(psi.x^2)/(mean(rho.x)^2)
-    nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
-
-    ## Value of the test statistic
-    statistic <- (est.x - est.y) /
-      sqrt((n * robustbase::scaleTau2(x, consistency = FALSE)^2 * nu.x +
-              m * robustbase::scaleTau2(y, consistency = FALSE)^2 * nu.y) / (m * n))
-
-    # ## ___________________________________________________________________________
-    # ## Compute test statistic
-    # ## ___________________________________________________________________________
-
-    # ## M-estimates
-    # est.x <- robustbase::huberM(x, k = 1.5)
-    # est.y <- robustbase::huberM(y, k = 1.5)
-    #
-    # ## Estimator for \nu
-    # psi.x <- robustbase::Mpsi((x - est.x$mu)/est.x$s, psi = "huber", cc = 1.5)
-    # rho.x <- robustbase::Mpsi((x - est.x$mu)/est.x$s, psi = "huber", cc = 1.5, deriv = 1)
-    #
-    # psi.y <- robustbase::Mpsi((y - est.y$mu)/est.y$s, psi = "huber", cc = 1.5)
-    # rho.y <- robustbase::Mpsi((y - est.y$mu)/est.y$s, psi = "huber", cc = 1.5, deriv = 1)
-    #
-    # #nu.x <- 2 * (k^2 * (1 - pnorm(k)) + pnorm(k) - 0.5 - k * dnorm(k))/(pnorm(k) - pnorm(-k))^2 #
-    # nu.x <- mean(psi.x^2)/(mean(rho.x)^2)
-    # nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
-    #
-    # #res[i] <- sqrt((m * n)/(m + n)) * (est.x$mu - est.y$mu)/((nu.x + nu.y)/2)
-    # statistic <- (est.x$mu - est.y$mu) /
-    #   sqrt((n * robustbase::scaleTau2(x, consistency = FALSE)^2 * nu.x +
-    #           m * robustbase::scaleTau2(y, consistency = FALSE)^2 * nu.y) / (m * n))
-    # #
-    # ## ___________________________________________________________________________
-    # ## Test decision
-    # ## ___________________________________________________________________________
-    # p.value <- switch (alternative,
-    #                    two.sided = 2 * stats::pnorm(abs(statistic), lower.tail = FALSE),
-    #                    greater = stats::pnorm(statistic, lower.tail = FALSE),
-    #                    less = stats::pnorm(statistic, lower.tail = TRUE)
-    # )
+    ## p-value
+    p.value <- switch (alternative,
+                       two.sided = 2 * stats::pnorm(abs(statistic), lower.tail = FALSE),
+                       greater = stats::pnorm(statistic, lower.tail = FALSE),
+                       less = stats::pnorm(statistic, lower.tail = TRUE)
+    )
   }
 
-  # if (var.test) {
-  #   names(estimates) <- c("M-est. of log(x^2)", "M-est. of log(y^2)")
-  #   names(delta) <- "ratio of variances"
-  #   delta <- exp(delta)
-  # } else {
-  #   names(estimates) <- c("M-est. of x", "M-est. of y")
-  #   names(delta) <- "location shift"
-  # }
-  # names(statistic) <- ifelse(var.test, "S", "D")
-  #
-  # if (method == "randomization") {
-  #   method = paste("Randomization test based on the ", paste0(toupper(substring(psi, 1, 1)), substring(psi, 2, nchar(psi))), "M-estimator")
-  # } else method = paste("Exact permutation test based on the", psi, "M-estimator")
+  ## ___________________________________________________________________________
+  ## Specify output
+  ## ___________________________________________________________________________
 
+  ## Names of data sets
+  dname <- paste(deparse(substitute(x)), "and", deparse(substitute(y)))
+
+  ## Names of estimates
+  if (var.test) {
+    names(estimates) <- c("M-est. of log(x^2)", "M-est. of log(y^2)")
+    names(delta) <- "ratio of variances"
+    delta <- exp(delta)
+  } else {
+    names(estimates) <- c("M-est. of x", "M-est. of y")
+    names(delta) <- "location shift"
+  }
+  names(statistic) <- ifelse(var.test, "S", "D")
+
+  ## Name of method to compute p-value
+  if (method == "randomization") {
+    method = paste("Randomization test based on the ", paste0(toupper(substring(psi, 1, 1)), substring(psi, 2, nchar(psi))), "M-estimator")
+  } else method = paste("Exact permutation test based on the", psi, "M-estimator")
+
+  ## Output
   res <- list(statistic = statistic, parameter = NULL, p.value = p.value,
               estimate = estimates, null.value = delta, alternative = alternative,
               method = method, data.name = dname)
@@ -202,84 +238,3 @@ m_test <- function(x, y, alternative = c("two.sided", "greater", "less"),
 
   return(res)
 }
-
-res <- numeric(1000)
-
-set.seed(108)
-for (i in 1:1000) {
-  x <- rnorm(500)
-  y <- rnorm(500)
-
-  res[i] <- m_test(x = x, y = y, alternative = "two.sided", method = "asymptotic")$statistic
-
-}
-
-
-res <- numeric(10000)
-
-m <- n <- 100
-
-
-for (i in 1:10000) {
-  x <- rt(m, df = 3)
-  y <- rt(n, df = 3)
-
-  ## M-estimates for both samples
-  est.x <- m_est(x = x, psi = psi, k = k)
-  est.y <- m_est(x = y, psi = psi, k = k)
-
-  ## Estimator for \nu
-  psi.x1 <- robustbase::Mpsi((x - est.x$est)/mad(x), psi = psi, cc = k)
-  rho.x <- robustbase::Mpsi((x - est.x$est)/mad(x), psi = psi, cc = k, deriv = 1)
-
-  psi.y <- robustbase::Mpsi((y - est.y$est)/mad(y), psi = psi, cc = k)
-  rho.y <- robustbase::Mpsi((y - est.y$est)/mad(y), psi = psi, cc = k, deriv = 1)
-
-  nu.x <- mean(psi.x^2)/(mean(rho.x)^2)
-  nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
-
-  #res[i] <- sqrt((m * n)/(m + n)) * (est.x$mu - est.y$mu)/((nu.x + nu.y)/2)
-  res[i] <- (est.x$est - est.y$est) /
-    sqrt((n * robustbase::scaleTau2(x, consistency = FALSE)^2 * nu.x +
-            m * robustbase::scaleTau2(y, consistency = FALSE)^2 * nu.y) / (m * n))
-}
-
-
-
-
-res <- numeric(10000)
-
-m <- n <- 100
-
-
-for (i in 1:10000) {
-  x <- rnorm(m)
-  y <- rnorm(n)
-
-  ## M-estimates
-  est.x <- huberM(x, k = 1.5)
-  est.y <- huberM(y, k = 1.5)
-
-  ## Estimator for \nu
-  psi.x <- robustbase::Mpsi((x - est.x$mu)/est.x$s, psi = "huber", cc = 1.5)
-  rho.x <- robustbase::Mpsi((x - est.x$mu)/est.x$s, psi = "huber", cc = 1.5, deriv = 1)
-
-  psi.y <- robustbase::Mpsi((y - est.y$mu)/est.y$s, psi = "huber", cc = 1.5)
-  rho.y <- robustbase::Mpsi((y - est.y$mu)/est.y$s, psi = "huber", cc = 1.5, deriv = 1)
-
-  #nu.x <- 2 * (k^2 * (1 - pnorm(k)) + pnorm(k) - 0.5 - k * dnorm(k))/(pnorm(k) - pnorm(-k))^2 #
-  nu.x <- mean(psi.x^2)/(mean(rho.x)^2)
-  nu.y <- mean(psi.y^2)/(mean(rho.y)^2)
-
-  #res[i] <- sqrt((m * n)/(m + n)) * (est.x$mu - est.y$mu)/((nu.x + nu.y)/2)
-  res[i] <- (est.x$mu - est.y$mu)/sqrt((n * scaleTau2(x, sigma0=mad(x))^2 * nu.x + m * scaleTau2(y, sigma0 = mad(y))^2 * nu.y)/(m * n))
-}
-
-
-hist(res, freq = FALSE, xlim = c(-5, 5), ylim = c(0, 0.5))
-curve(dnorm(x, sd = 1), from = -5, to = 5, add = TRUE)
-
-
-qqnorm(res)
-
-
