82a228db5c827c8faf052905721044443c00cc34
s-abbas <sermad.abbas@tu-dortmund.de>
2020-10-14 21:47:45 +0200

    Updated templates for some function arguments.

--- man-roxygen/wobble.R
+++ man-roxygen/wobble.R
@@ -1,4 +1,5 @@
-#' @param wobble.seed logical value indicating whether uniform noise should be added to the sample before
+#' @param wobble logical value indicating whether uniform noise should be added to the sample before
 #'               proceeding with the test, in order to make sure there are no duplicate values in the
 #'               sample that can cause the scale estimate to be 0. Only necessary for the permutation and
 #'               randomization version of the test. Defaults to FALSE.
+#' @param wobble.seed integer value used as a seed for the random number generation in case of \code{wobble = TRUE}.
