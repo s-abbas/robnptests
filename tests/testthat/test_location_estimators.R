ff508f294ae735f7827ff212b6e78026b39d386b
s-abbas <sermad.abbas@tu-dortmund.de>
2021-02-27 21:10:36 +0100

    test
    Added unit tests for function trim_mean in test_location_estimators.R.

--- tests/testthat/test_location_estimators.R
+++ tests/testthat/test_location_estimators.R
@@ -1,4 +1,46 @@
-context("Location estimators")
+
+## Trimmed mean ----
+testthat::test_that("trimmed_mean works correctly", {
+
+  ## Generate exemplary input vector
+  x <- c(1, 3, 8, 9, 15)
+
+  ## The input arguments need to be checked
+
+  # Missing input arguments
+  testthat::expect_error(trim_mean(gamma = 0.2, na.rm = FALSE))
+  testthat::expect_equal(trim_mean(x = x, na.rm = FALSE), trim_mean(x = x, gamma = 0.2, na.rm = FALSE))
+  testthat::expect_equal(trim_mean(x = x, gamma = 0.2), trim_mean(x = x, gamma = 0.2, na.rm = FALSE))
+
+  # Wrong data types
+  testthat::expect_error(trim_mean(x = as.character(x), gamma = 0.2, na.rm = FALSE))
+  testthat::expect_error(trim_mean(x = x, gamma = as.character(0.2), na.rm = FALSE))
+  testthat::expect_error(trim_mean(x = x, gamma = 0.2, na.rm = 1))
+  testthat::expect_error(trim_mean(x = x, gamma = 0.7, na.rm = FALSE), regexp = "'gamma' has to be in [0, 0.5].", fixed = TRUE)
+  testthat::expect_error(trim_mean(x = x, gamma = -0.01, na.rm = FALSE), regexp = "'gamma' has to be in [0, 0.5].", fixed = TRUE)
+
+  ## Missing values should be removed correctly
+  testthat::expect_equal(trim_mean(x = c(x, NA), gamma = 0.2, na.rm = TRUE),
+                         trim_mean(x = x, gamma = 0.2, na.rm = TRUE)
+  )
+  testthat::expect_equal(trim_mean(x = c(x, NA), gamma = 0.2, na.rm = FALSE), NA_real_)
+  testthat::expect_equal(trim_mean(x = c(x, NA), gamma = 0.2),
+                         trim_mean(x = c(x, NA), gamma = 0.2, na.rm = FALSE)
+  )
+
+  ## The computed value need to be identical to the output of mean(x, trim = ...)
+  testthat::expect_equal(trim_mean(x = x, gamma = 0.2),
+                         mean(x = x, trim = 0.2)
+  )
+  testthat::expect_equal(trim_mean(x = x, gamma = 0),
+                         mean(x = x, trim = 0)
+  )
+  testthat::expect_equal(trim_mean(x = x, gamma = 0.5),
+                         mean(x = x, trim = 0.5)
+  )
+})
+
+
 
 ## One-sample Hodges-Lehmann estimator
 testthat::test_that("hodges_lehmann works correctly", {
