0223dbfa083906baba36480b81d67146f415fdc0
s-abbas <sermad.abbas@tu-dortmund.de>
2021-03-07 21:37:38 +0100

    test
    Added and revised unit tests for functions win_var and rob_var in test_scale_estimators.R.

--- tests/testthat/test_scale_estimators.R
+++ tests/testthat/test_scale_estimators.R
@@ -1,111 +1,186 @@
-context("Scale Estimators")
+
+## Winsorized variance ----
+testthat::test_that("win_var works correctly", {
+
+  ## Generate exemplary input vector
+  x <- c(19, 72, 51, 43, 87, 91, 50, 38, 4, 63)
+  x1 <- c(19, 72, 51, 43, 87, 91, 50, 38, 4, 63, 75)
+
+  ## Checks for input arguments ----
+  # 'x'
+  testthat::expect_error(win_var(), regexp = "'x' is missing.", fixed = TRUE)
+  testthat::expect_error(win_var(c("a", "b", "c")), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
+  testthat::expect_error(win_var(x[1]), regexp = "Assertion on 'x' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
+  testthat::expect_error(win_var(c(x, Inf)), regexp = "Assertion on 'x' failed: Must be finite.", fixed = TRUE)
+  testthat::expect_error(win_var(rep(NA, 2)), regexp = "Assertion on 'x' failed: Contains only missing values.", fixed = TRUE)
+  testthat::expect_error(win_var(NULL), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
+
+  # 'gamma'
+  testthat::expect_error(win_var(x, gamma = "0.5"), regexp = "Assertion on 'gamma' failed: Must be of type 'number', not 'character'.", fixed = TRUE)
+  testthat::expect_error(win_var(x, gamma = -1), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
+  testthat::expect_error(win_var(x, gamma = 1), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
+  testthat::expect_error(win_var(x, gamma = NA), regexp = "Assertion on 'gamma' failed: May not be NA.", fixed = TRUE)
+  testthat::expect_error(win_var(x, gamma = c(0.2, 0.5)), regexp = "Assertion on 'gamma' failed: Must have length 1.", fixed = TRUE)
+  testthat::expect_error(win_var(x, gamma = NULL), regexp = "Assertion on 'gamma' failed: Must be of type 'number', not 'NULL'.", fixed = TRUE)
+
+  # 'na.rm'
+  testthat::expect_error(win_var(x, na.rm = 1), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'double'.", fixed = TRUE)
+  testthat::expect_error(win_var(x, na.rm = NA), regexp = "Assertion on 'na.rm' failed: May not be NA.", fixed = TRUE)
+  testthat::expect_error(win_var(x, na.rm = c(TRUE, FALSE)), regexp = "Assertion on 'na.rm' failed: Must have length 1.", fixed = TRUE)
+  testthat::expect_error(win_var(x, na.rm = NULL), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'NULL'.", fixed = TRUE)
+
+  ## Missing values should be removed correctly ----
+  testthat::expect_equal(win_var(x = c(x, NA), na.rm = TRUE), win_var(x = x))
+  testthat::expect_equal(win_var(x = c(x, NA)), list(var = NA_real_, h = NA_real_))
+  testthat::expect_equal(win_var(x = c(x, NA)), win_var(x = c(x, NA), na.rm = FALSE))
+
+  ## The estimator should be location and permutation invariant ----
+  # Location invariance
+  testthat::expect_equal(win_var(x = x + 5, gamma = 0), win_var(x = x, gamma = 0))
+  testthat::expect_equal(win_var(x = x + 5, gamma = 0.25), win_var(x = x, gamma = 0.25))
+
+  # Scale equivariance
+  testthat::expect_equal(win_var(x = 2 * x, gamma = 0), list(var = 4 * win_var(x, gamma = 0)$var, h = 10))
+  testthat::expect_equal(win_var(x = 2 * x, gamma = 0.25), list(var = 4 * win_var(x, gamma = 0.25)$var, h = 6))
+
+  # Permutation invariance
+  testthat::expect_equal(win_var(x = x, gamma = 0), win_var(x = sort(x), gamma = 0))
+  testthat::expect_equal(win_var(x = x, gamma = 0.25), win_var(x = sort(x), gamma = 0.25))
+
+  ## Check output ----
+
+  # For 'gamma' = 0, the winsorized variance of 'x' is equal to the sample variance
+  testthat::expect_equal(win_var(x = x), list(var = var(x), h = length(x)))
+
+  # For 'gamma' = 0.2, the winsorized variance of 'x' is equal to 218.4556
+  testthat::expect_equal(win_var(x = x, gamma = 0.2), list(var = 218.4556, h = 6), tol = 1e-6)
+
+  # For 'gamma' = 0.3, the winsorized variance of 'x' is equal to 90.05556
+  testthat::expect_equal(win_var(x = x, gamma = 0.3), list(var = 90.05556, h = 4), tol = 1e-6)
+
+  # For 'gamma' = 0.25, the winsorized variance of 'x1' is equal to 90.05556
+  testthat::expect_equal(win_var(x = x1, gamma = 0.25), list(var = 258.9636, h = 7), tol = 1e-6)
+
+  # The output should be a list of numeric scalars
+  checkmate::expect_list(win_var(x = x), types = rep("numeric", 2))
+})
 
 testthat::test_that("Robust scale estimators work correctly", {
 
-  testthat::skip_on_cran()
+  # testthat::skip_on_cran()
 
-  ##
-  ## Check that the function returns errors when we hand over the wrong method
-  ## or something non-numeric
-  ##
+  ## Generate exemplary input vector
+  x <- c(7, 2, 1, 6, 8)
+  y <- c(5, 9, 6, 7, 8)
 
-  set.seed(108)
+  ## Checks for input arguments ----
+  # 'x'
+  testthat::expect_error(rob_var(), regexp = "'x' is missing.", fixed = TRUE)
+  testthat::expect_error(rob_var(y = y), regexp = "'x' is missing.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = c("a", "b", "c"), y = y), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x[1], y = y), regexp = "Assertion on 'x' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = c(x, Inf), y  = y), regexp = "Assertion on 'x' failed: Must be finite.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = rep(NA, 2), y = y), regexp = "Assertion on 'x' failed: Contains only missing values.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = NULL, y = y), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
 
-  x <- rnorm(10)
-  y <- rnorm(10)
+  # 'y'
+  testthat::expect_error(rob_var(x = x), regexp = "'y' is missing.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = c("a", "b", "c")), regexp = "Assertion on 'y' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = y[1]), regexp = "Assertion on 'y' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = c(y, Inf)), regexp = "Assertion on 'y' failed: Must be finite.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = rep(NA, 2)), regexp = "Assertion on 'y' failed: Contains only missing values.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = NULL), regexp = "Assertion on 'y' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
 
-  testthat::expect_error(rob_var(x, y, na.rm=FALSE, type="not implemented"))
-  testthat::expect_error(rob_var(x, y, na.rm=FALSE, type=1))
-  testthat::expect_error(rob_var(letters[1:10], y, na.rm=FALSE))
+  # 'type'
+  testthat::expect_error(rob_var(x = x, y = y, type = 1), regexp = "Assertion on 'type' failed: Must be of type 'character', not 'double'.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = y, type = c("S1", "S2")), regexp = "Assertion on 'type' failed: Must be element of set {'S1','S2','S3','S4'}, but is not atomic scalar.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = y, type = "T2"), regexp = "Assertion on 'type' failed: Must be element of set {'S1','S2','S3','S4'}, but is 'T2'.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = y, type = NA), regexp = "Assertion on 'type' failed: Contains only missing values.", fixed = TRUE)
+  testthat::expect_error(rob_var(x = x, y = y, type = NULL), regexp = "Assertion on 'type' failed: Must be of type 'character', not 'NULL'.", fixed = TRUE)
 
-  ##
-  ## NA handling
-  ##
+  # 'na.rm'
+  testthat::expect_error(rob_var(x, y, na.rm = 1), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'double'.", fixed = TRUE)
+  testthat::expect_error(rob_var(x, y, na.rm = NA), regexp = "Assertion on 'na.rm' failed: May not be NA.", fixed = TRUE)
+  testthat::expect_error(rob_var(x, y, na.rm = c(TRUE, FALSE)), regexp = "Assertion on 'na.rm' failed: Must have length 1.", fixed = TRUE)
+  testthat::expect_error(rob_var(x, y, na.rm = NULL), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'NULL'.", fixed = TRUE)
 
-  types <- c("S1", "S2", "S3", "S4")
+  ## Missing values should be removed correctly ----
+  testthat::expect_equal(rob_var(x = c(x, NA), y = y, na.rm = TRUE), rob_var(x = x, y = y))
+  testthat::expect_equal(rob_var(x = c(x, NA), y = y), NA_real_)
+  testthat::expect_equal(rob_var(x = c(x, NA), y = y), rob_var(x = c(x, NA), y = y, na.rm = FALSE))
 
-  x2 <- c(x, NA)
+  testthat::expect_equal(rob_var(x = x, y = c(y, NA), na.rm = TRUE), rob_var(x = x, y = y))
+  testthat::expect_equal(rob_var(x = x, y = c(y, NA)), NA_real_)
+  testthat::expect_equal(rob_var(x = x, y = c(y, NA)), rob_var(x = x, y = c(y, NA), na.rm = FALSE))
 
-  for (type in types) {
-    testthat::expect_true(is.na(rob_var(x2, y, na.rm=FALSE, type=type)))
-    testthat::expect_identical(rob_var(x, y, na.rm=FALSE, type=type),
-                               rob_var(x2, y, na.rm=TRUE, type=type))
-  }
+  testthat::expect_equal(rob_var(x = c(x, NA), y = c(y, NA), na.rm = TRUE), rob_var(x = x, y = y))
+  testthat::expect_equal(rob_var(x = c(x, NA), y = c(y, NA)), NA_real_)
+  testthat::expect_equal(rob_var(x = c(x, NA), y = c(y, NA)), rob_var(x = c(x, NA), y = c(y, NA)), na.rm = FALSE)
 
-  ##
-  ## Check for location and permutation invariance
-  ##
+  ## The estimator should be location and permutation invariant ----
+  # Location invariance
+  testthat::expect_equal(rob_var(x = x + 5, y = y, type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = x + 5, y = y, type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = x + 5, y = y, type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = x + 5, y = y, type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-  x3 <- x + 5 # shift in location
-  x4 <- sort(x) # a simple 'permutation' of x
+  testthat::expect_equal(rob_var(x = x, y = y + 5, type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = x, y = y + 5, type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = x, y = y + 5, type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = x, y = y + 5, type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-  for (type in types) {
-    testthat::expect_equal(rob_var(x, y, type=type), rob_var(y, x, type=type)) # switching x and y
-    testthat::expect_equal(rob_var(x, y, type=type), rob_var(x3, y, type=type)) # location invariance
-    testthat::expect_equal(rob_var(x, y, type=type), rob_var(x4, y, type=type)) # permutation invariance
-  }
+  testthat::expect_equal(rob_var(x = x + 2, y = y + 3, type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = x + 2, y = y + 3, type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = x + 2, y = y + 3, type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = x + 2, y = y + 3, type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-  ##
-  ## Check if the function returns 0 when all observations are constant
-  ## and prints a warning if the observations are not.
-  ##
+  # Permutation invariance
+  testthat::expect_equal(rob_var(x = sort(x), y = y, type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = sort(x), y = y, type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = sort(x), y = y, type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = sort(x), y = y, type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-  for (type in types) {
-    testthat::expect_equal(suppressWarnings(rob_var(rep(1, 10), rep(1, 10), type=type)), 0)
-    testthat::expect_warning(rob_var(x, rep(1, 10), type=type))
-  }
+  testthat::expect_equal(rob_var(x = x, y = sort(y), type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = x, y = sort(y), type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = x, y = sort(y), type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = x, y = sort(y), type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-  testthat::expect_error(rob_var(rep(c(1, 2), 5), c(rep(1, 9), 100), type="S1"))
+  testthat::expect_equal(rob_var(x = sort(x), y = sort(y), type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = sort(x), y = sort(y), type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = sort(x), y = sort(y), type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = sort(x), y = sort(y), type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-}
-)
+  # Scale equivariance
+  testthat::expect_equal(rob_var(x = 2 * x, y = 2 * y, type = "S1"), 2 * rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = 2 * x, y = 2 * y, type = "S2"), 2 * rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = 2 * x, y = 2 * y, type = "S3"), 2 * rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = 2 * x, y = 2 * y, type = "S4"), 2 * rob_var(x = x, y = y, type = "S4"))
 
+  # Switch 'x' and 'y'
+  testthat::expect_equal(rob_var(x = y, y = x, type = "S1"), rob_var(x = x, y = y, type = "S1"))
+  testthat::expect_equal(rob_var(x = y, y = x, type = "S2"), rob_var(x = x, y = y, type = "S2"))
+  testthat::expect_equal(rob_var(x = y, y = x, type = "S3"), rob_var(x = x, y = y, type = "S3"))
+  testthat::expect_equal(rob_var(x = y, y = x, type = "S4"), rob_var(x = x, y = y, type = "S4"))
 
-testthat::test_that("Winsorized variance works correctly", {
+  ## Check output ----
 
-  ##
-  ## Check that the function returns errors when we hand over the wrong parameter
-  ## or something non-numeric
-  ##
+  # For 'type' = 'S1', the output should be 2
+  testthat::expect_equal(rob_var(x = x, y = y, type = "S1"), 2)
 
-  set.seed(108)
+  # For 'type' = 'S2', the output should be 2
+  testthat::expect_equal(rob_var(x = x, y = y, type = "S2"), 2)
 
-  x <- rnorm(10)
-  y <- rnorm(10)
+  # For 'type' = 'S3', the output should be 3
+  testthat::expect_equal(rob_var(x = x, y = y, type = "S3"), 3)
 
-  testthat::expect_error(win_var(x, gamma=-0.2))
-  testthat::expect_error(win_var(x, gamma="none"))
+  # For 'type' = 'S4', the output should be 3
+  testthat::expect_equal(rob_var(x = x, y = y, type = "S4"), 3)
 
-  ##
-  ## NA handling
-  ##
-
-  x2 <- c(x, NA)
-
-  testthat::expect_true(is.na(win_var(x2, gamma=0)))
-  testthat::expect_equal(win_var(x, gamma=0), win_var(x2, gamma=0, na.rm=TRUE))
-  testthat::expect_equal(win_var(x, gamma=0.25), win_var(x2, gamma=0.25, na.rm=TRUE))
-
-  ##
-  ## Check for location and permutation invariance
-  ##
-
-  x3 <- x + 5
-  x4 <- sort(x)
-
-  testthat::expect_equal(win_var(x, gamma=0), win_var(x3, gamma=0, na.rm=TRUE))
-  testthat::expect_equal(win_var(x, gamma=0), win_var(x4, gamma=0, na.rm=TRUE))
-  testthat::expect_equal(win_var(x, gamma=0.25), win_var(x3, gamma=0.25, na.rm=TRUE))
-  testthat::expect_equal(win_var(x, gamma=0.25), win_var(x4, gamma=0.25, na.rm=TRUE))
-
-  ##
-  ## Check whether different values of gamma yield different estimates
-  ##
-
-  g1 <- win_var(x, gamma=0)$var
-  g2 <- win_var(x, gamma=0.1)$var
-
-  testthat::expect_true(g1 != g2)
-
+  # The output should be a numeric scalar
+  checkmate::expect_numeric(rob_var(x = x, y = y, type = "S1"), len = 1)
+  checkmate::expect_numeric(rob_var(x = x, y = y, type = "S2"), len = 1)
+  checkmate::expect_numeric(rob_var(x = x, y = y, type = "S3"), len = 1)
+  checkmate::expect_numeric(rob_var(x = x, y = y, type = "S4"), len = 1)
 })
+
 
