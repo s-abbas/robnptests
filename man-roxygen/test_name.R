0f1035681b8955758059bb6604b707cbec8429da
s-abbas <sermad.abbas@tu-dortmund.de>
2021-03-28 19:42:14 +0200

    documentation
    Added documentation for arguments 'test.name' ('test_name.R') and 'wobble.seed' ('wobble_seed.R').
    
    Documentation for 'wobble.seed' now moved to own file 'wobble_seed.R' and removed from 'wobble.R'.

---
+++ man-roxygen/test_name.R
@@ -0,0 +1,2 @@
+#' @param test.name character string specifying the two-sample test for which
+#'                  the helper function is used.
---
+++ man-roxygen/wobble_seed.R
@@ -0,0 +1,6 @@
+#' @param wobble.seed integer value used as a seed for the random number
+#'                    generation in case of \code{wobble = TRUE} or when the
+#'                    \code{var.test = TRUE} and on of the vectors \code{x} and
+#'                    \code{y} contains zeros. When no seed is specified, it is
+#'                    set randomly. The argument is ignored if
+#'                    \code{var.test = FALSE} and \code{wobble = FALSE}.
