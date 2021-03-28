b892dfbbddc2aab45eaa84f966737ddcc73f3dba
s-abbas <sermad.abbas@tu-dortmund.de>
2021-03-28 19:48:11 +0200

    test
    Removed tests for checks of input arguments in 'test_location_estimators.R', 'test_permutation_distribution.R', and 'test_scale_estimators.R'.
    
    Tests will only be performed for function 'check_test_input' ('helpers.R').

--- tests/testthat/test_location_estimators.R
+++ tests/testthat/test_location_estimators.R
@@ -5,31 +5,6 @@ testthat::test_that("trimmed_mean works correctly", {
   ## Generate exemplary input vector
   x <- c(1, 3, 8, 9, 15)
 
-  ## The input arguments need to be checked
-
-  # Missing input arguments
-  testthat::expect_error(trim_mean(gamma = 0.2, na.rm = FALSE), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_equal(trim_mean(x = x, na.rm = FALSE), trim_mean(x = x, gamma = 0.2, na.rm = FALSE))
-  testthat::expect_equal(trim_mean(x = x, gamma = 0.2), trim_mean(x = x, gamma = 0.2, na.rm = FALSE))
-
-  # Input arguments are NULL
-  testthat::expect_error(trim_mean(x = NULL, gamma = 0.2, na.rm = FALSE), regexp = "'x' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = NULL, na.rm = FALSE), regexp = "'gamma' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = 0.2, na.rm = NULL), regexp = "'na.rm' must not be NULL.", fixed = TRUE)
-
-  # Wrong data types
-  testthat::expect_error(trim_mean(x = as.character(x), gamma = 0.2, na.rm = FALSE), regexp = "'x' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = as.character(0.2), na.rm = FALSE), regexp = "'gamma' has to be a numeric value.", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = 0.2, na.rm = 1), regexp = "'na.rm' has to be a logical value.", fixed = TRUE)
-
-  # Length of input arguments
-  testthat::expect_error(trim_mean(x = x, gamma = rep(0.1, 3)), regexp = "'gamma' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = 0.2, na.rm = rep(TRUE, 3)), regexp = "'na.rm' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-
-  # Wrong value for 'gamma'
-  testthat::expect_error(trim_mean(x = x, gamma = 0.7, na.rm = FALSE), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-  testthat::expect_error(trim_mean(x = x, gamma = -0.01, na.rm = FALSE), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-
   ## Missing values should be removed correctly
   testthat::expect_equal(trim_mean(x = c(x, NA), gamma = 0.2, na.rm = TRUE),
                          trim_mean(x = x, gamma = 0.2, na.rm = TRUE)
@@ -61,31 +36,6 @@ testthat::test_that("win_mean works correctly", {
   ## Generate exemplary input vector
   x <- c(92, 19, 101, 58, 1053, 91, 26, 78, 10, 13, -40, 101, 86, 85, 15, 89, 89, 28, -5, 41)
 
-  ## The input arguments need to be checked
-
-  # Missing input arguments
-  testthat::expect_error(win_mean(gamma = 0.2, na.rm = FALSE), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_equal(win_mean(x = x, na.rm = FALSE), win_mean(x = x, gamma = 0.2, na.rm = FALSE))
-  testthat::expect_equal(win_mean(x = x, gamma = 0.2), win_mean(x = x, gamma = 0.2, na.rm = FALSE))
-
-  # Input arguments are NULL
-  testthat::expect_error(win_mean(x = NULL, gamma = 0.2, na.rm = FALSE), regexp = "'x' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = NULL, na.rm = FALSE), regexp = "'gamma' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = 0.2, na.rm = NULL), regexp = "'na.rm' must not be NULL.", fixed = TRUE)
-
-  # Wrong data types
-  testthat::expect_error(win_mean(x = as.character(x), gamma = 0.2, na.rm = FALSE), regexp = "'x' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = as.character(0.2), na.rm = FALSE), regexp = "'gamma' has to be a numeric value.", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = 0.2, na.rm = 1), regexp = "'na.rm' has to be a logical value.", fixed = TRUE)
-
-  # Length of input arguments
-  testthat::expect_error(win_mean(x = x, gamma = rep(0.1, 3)), regexp = "'gamma' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = 0.2, na.rm = rep(TRUE, 3)), regexp = "'na.rm' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-
-  # Wrong value for 'gamma'
-  testthat::expect_error(win_mean(x = x, gamma = 0.7, na.rm = FALSE), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-  testthat::expect_error(win_mean(x = x, gamma = -0.01, na.rm = FALSE), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-
   ## Missing values should be removed correctly
   testthat::expect_equal(win_mean(x = c(x, NA), gamma = 0.2, na.rm = TRUE),
                          win_mean(x = x, gamma = 0.2, na.rm = TRUE)
@@ -111,23 +61,6 @@ testthat::test_that("hodges_lehmann works correctly", {
   ## Generate exemplary input vector
   x <- c(1, 3, 8, 9, 15)
 
-  ## The input arguments need to be checked
-
-  # Missing input arguments
-  testthat::expect_error(hodges_lehmann(na.rm = FALSE), regexp = "'x' is missing.", fixed = TRUE)
-
-  # Input arguments are NULL
-  testthat::expect_error(hodges_lehmann(x = NULL, na.rm = FALSE), regexp = "'x' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann(x = x, na.rm = NULL), regexp = "'na.rm' must not be NULL.", fixed = TRUE)
-
-  # Wrong data types
-  testthat::expect_error(hodges_lehmann(x = as.character(x), na.rm = FALSE), regexp = "'x' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann(x = x, na.rm = 1), regexp = "'na.rm' has to be a logical value.", fixed = TRUE)
-
-  # Length of input arguments
-  testthat::expect_error(hodges_lehmann(x = x[1], na.rm = FALSE), regexp = "'x' needs at least 2 values.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann(x = x, na.rm = rep(TRUE, 3)), regexp = "'na.rm' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-
   ## Missing values should be removed correctly
   testthat::expect_equal(hodges_lehmann(x = c(x, NA), na.rm = TRUE),
                          hodges_lehmann(x = x, na.rm = TRUE)
@@ -152,25 +85,6 @@ testthat::test_that("hodges_lehmann_2sample works correctly", {
   m <- length(x)
   y <- c(32, 3, 4, 8, 13, 12, 17, 20, 3, 11)
   n <- length(y)
-
-  ## The input arguments need to be checked
-
-  # Missing input arguments
-  testthat::expect_error(hodges_lehmann_2sample(y = y, na.rm = FALSE), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann_2sample(x = x, na.rm = FALSE), regexp = "'y' is missing.", fixed = TRUE)
-
-  # Input arguments are NULL
-  testthat::expect_error(hodges_lehmann_2sample(x = NULL, y = y, na.rm = FALSE), regexp = "'x' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann_2sample(x = x, y = NULL, na.rm = FALSE), regexp = "'y' must not be NULL.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann_2sample(x = x, y = y, na.rm = NULL), regexp = "'na.rm' must not be NULL.", fixed = TRUE)
-
-  # Wrong data types
-  testthat::expect_error(hodges_lehmann_2sample(x = as.character(x), y = y, na.rm = FALSE), regexp = "'x' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann_2sample(x = x, y = as.character(y), na.rm = FALSE), regexp = "'y' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(hodges_lehmann_2sample(x = x, y = y, na.rm = 1), regexp = "'na.rm' has to be a logical value.", fixed = TRUE)
-
-  # Length of input arguments
-  testthat::expect_error(hodges_lehmann_2sample(x = x, y = y, na.rm = rep(TRUE, 3)), regexp = "'na.rm' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
 
   ## Missing values should be removed correctly
   testthat::expect_equal(hodges_lehmann_2sample(x = c(x, NA), y = y, na.rm = TRUE),
@@ -200,48 +114,6 @@ testthat::test_that("hodges_lehmann_2sample works correctly", {
 
 ## M-estimator ----
 testthat::test_that("m_est works correctly", {
-
-  ## The input arguments need to be checked
-
-  # Generate exemplary input vector
-  set.seed(108)
-  x <- rnorm(10)
-
-  # Missing input arguments
-  testthat::expect_error(m_est(psi = "huber"), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x), regexp = "'psi' is missing.", fixed = TRUE)
-
-  # Input arguments are NULL
-  testthat::expect_error(m_est(x = NULL, psi = "huber"), regexp = "'x' must not be NULL", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = NULL), regexp = "'psi' must not be NULL", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", k = NULL), regexp = "'k' must not be NULL", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", tol = NULL), regexp = "'tol' must not be NULL", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", max.it = NULL), regexp = "'max.it' must not be NULL", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", na.rm = NULL), regexp = "'na.rm' must not be NULL", fixed = TRUE)
-
-  # Input arguments are NA
-  testthat::expect_error(m_est(x = x, psi = NA)) # An error is thrown by .Mpsi.tuning.default(psi)
-  testthat::expect_error(m_est(x = x, psi = "huber", k = NA), regexp = "'k' must not be NA", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", tol = NA), regexp = "'tol' must not be NA", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", max.it = NA), regexp = "'max.it' must not be NA", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", na.rm = NA), regexp = "'na.rm' must not be NA", fixed = TRUE)
-
-  # Wrong data types
-  testthat::expect_error(m_est(x = as.character(x), psi = "huber"), regexp = "'x' has to a numeric vector.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = 1), regexp = "'psi' has to a character value.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", k = "1.345"), regexp = "'k' has to be a numeric value.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", tol = "1e-06"), regexp = "'tol' has to be a numeric value.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", max.it = "15"), regexp = "'max.it' has to be a numeric value.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", na.rm = 1), regexp = "'na.rm' has to be a logical value.", fixed = TRUE)
-
-  # Length of input arguments
-  testthat::expect_error(m_est(x = x, psi = c("huber", "hampel", "bisquare"))) # An error is thrown by .Mpsi.tuning.defaul(psi)
-  testthat::expect_error(m_est(x = x, psi = "welsh"), regexp = "'psi' must be one of 'huber', 'hampel', or 'bisquare'.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = c("tukey"))) # An error is thrown by .Mpsi.tuning.defaul(psi)
-  testthat::expect_error(m_est(x = x, psi = "huber", k = c(1.03, 1.345)), regexp = "Coef. for psi function huber not of length 1", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", tol = c(1e-06, 1e-10)), regexp = "'tol' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", max.it = c(10, 15)), regexp = "'max.it' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
-  testthat::expect_error(m_est(x = x, psi = "huber", na.rm = c(TRUE, FALSE)), regexp = "'na.rm' has to be a single value, not a vector of length >= 1.", fixed = TRUE)
 
   ## Missing values should be removed correctly
   testthat::expect_equal(m_est(x = c(x, NA), psi = "huber", na.rm = TRUE),
--- tests/testthat/test_scale_estimators.R
+++ tests/testthat/test_scale_estimators.R
@@ -6,29 +6,6 @@ testthat::test_that("win_var works correctly", {
   x <- c(19, 72, 51, 43, 87, 91, 50, 38, 4, 63)
   x1 <- c(19, 72, 51, 43, 87, 91, 50, 38, 4, 63, 75)
 
-  ## Checks for input arguments ----
-  # 'x'
-  testthat::expect_error(win_var(), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_error(win_var(c("a", "b", "c")), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
-  testthat::expect_error(win_var(x[1]), regexp = "Assertion on 'x' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
-  testthat::expect_error(win_var(c(x, Inf)), regexp = "Assertion on 'x' failed: Must be finite.", fixed = TRUE)
-  testthat::expect_error(win_var(rep(NA, 2)), regexp = "Assertion on 'x' failed: Contains only missing values.", fixed = TRUE)
-  testthat::expect_error(win_var(NULL), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
-
-  # 'gamma'
-  testthat::expect_error(win_var(x, gamma = "0.5"), regexp = "Assertion on 'gamma' failed: Must be of type 'number', not 'character'.", fixed = TRUE)
-  testthat::expect_error(win_var(x, gamma = -1), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-  testthat::expect_error(win_var(x, gamma = 1), regexp = "'gamma' has to be a numeric value in [0, 0.5].", fixed = TRUE)
-  testthat::expect_error(win_var(x, gamma = NA), regexp = "Assertion on 'gamma' failed: May not be NA.", fixed = TRUE)
-  testthat::expect_error(win_var(x, gamma = c(0.2, 0.5)), regexp = "Assertion on 'gamma' failed: Must have length 1.", fixed = TRUE)
-  testthat::expect_error(win_var(x, gamma = NULL), regexp = "Assertion on 'gamma' failed: Must be of type 'number', not 'NULL'.", fixed = TRUE)
-
-  # 'na.rm'
-  testthat::expect_error(win_var(x, na.rm = 1), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'double'.", fixed = TRUE)
-  testthat::expect_error(win_var(x, na.rm = NA), regexp = "Assertion on 'na.rm' failed: May not be NA.", fixed = TRUE)
-  testthat::expect_error(win_var(x, na.rm = c(TRUE, FALSE)), regexp = "Assertion on 'na.rm' failed: Must have length 1.", fixed = TRUE)
-  testthat::expect_error(win_var(x, na.rm = NULL), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'NULL'.", fixed = TRUE)
-
   ## Missing values should be removed correctly ----
   testthat::expect_equal(win_var(x = c(x, NA), na.rm = TRUE), win_var(x = x))
   testthat::expect_equal(win_var(x = c(x, NA)), list(var = NA_real_, h = NA_real_))
@@ -72,37 +49,6 @@ testthat::test_that("Robust scale estimators work correctly", {
   ## Generate exemplary input vector
   x <- c(7, 2, 1, 6, 8)
   y <- c(5, 9, 6, 7, 8)
-
-  ## Checks for input arguments ----
-  # 'x'
-  testthat::expect_error(rob_var(), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_error(rob_var(y = y), regexp = "'x' is missing.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = c("a", "b", "c"), y = y), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x[1], y = y), regexp = "Assertion on 'x' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = c(x, Inf), y  = y), regexp = "Assertion on 'x' failed: Must be finite.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = rep(NA, 2), y = y), regexp = "Assertion on 'x' failed: Contains only missing values.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = NULL, y = y), regexp = "Assertion on 'x' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
-
-  # 'y'
-  testthat::expect_error(rob_var(x = x), regexp = "'y' is missing.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = c("a", "b", "c")), regexp = "Assertion on 'y' failed: Must be of type 'numeric', not 'character'.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = y[1]), regexp = "Assertion on 'y' failed: Must have length >= 2, but has length 1.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = c(y, Inf)), regexp = "Assertion on 'y' failed: Must be finite.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = rep(NA, 2)), regexp = "Assertion on 'y' failed: Contains only missing values.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = NULL), regexp = "Assertion on 'y' failed: Must be of type 'numeric', not 'NULL'.", fixed = TRUE)
-
-  # 'type'
-  testthat::expect_error(rob_var(x = x, y = y, type = 1), regexp = "Assertion on 'type' failed: Must be of type 'character', not 'double'.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = y, type = c("S1", "S2")), regexp = "Assertion on 'type' failed: Must be element of set {'S1','S2','S3','S4'}, but is not atomic scalar.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = y, type = "T2"), regexp = "Assertion on 'type' failed: Must be element of set {'S1','S2','S3','S4'}, but is 'T2'.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = y, type = NA), regexp = "Assertion on 'type' failed: Contains only missing values.", fixed = TRUE)
-  testthat::expect_error(rob_var(x = x, y = y, type = NULL), regexp = "Assertion on 'type' failed: Must be of type 'character', not 'NULL'.", fixed = TRUE)
-
-  # 'na.rm'
-  testthat::expect_error(rob_var(x, y, na.rm = 1), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'double'.", fixed = TRUE)
-  testthat::expect_error(rob_var(x, y, na.rm = NA), regexp = "Assertion on 'na.rm' failed: May not be NA.", fixed = TRUE)
-  testthat::expect_error(rob_var(x, y, na.rm = c(TRUE, FALSE)), regexp = "Assertion on 'na.rm' failed: Must have length 1.", fixed = TRUE)
-  testthat::expect_error(rob_var(x, y, na.rm = NULL), regexp = "Assertion on 'na.rm' failed: Must be of type 'logical flag', not 'NULL'.", fixed = TRUE)
 
   ## Missing values should be removed correctly ----
   testthat::expect_equal(rob_var(x = c(x, NA), y = y, na.rm = TRUE), rob_var(x = x, y = y))
