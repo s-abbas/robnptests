dec77cb602f05c130de58268abbe000aff2fcb63
s-abbas <sermad.abbas@tu-dortmund.de>
2021-03-28 19:44:25 +0200

    documentation
    Revised documentation for 'scale' ('scale_hl.R') and 'seed' ('seed.R'), and removed documentation for 'wobble.seed' from 'wobble.R'.

--- man-roxygen/seed.R
+++ man-roxygen/seed.R
@@ -1,3 +1,4 @@
-#' @param seed Optional, numerical value used as a seed to be used for 'wobbling' of the samples.
-#'             If no seed is given, it is set randomly. In any case, the seed used will be returned in a
-#'             warning message. The argument is ignored if \code{wobble = FALSE}.
+#' @param seed Optional, numerical value used as a seed for wobbling of the
+#'             samples. The argument is ignored if \code{wobble = FALSE}. If no
+#'             value is given, a random number is used. The seed  will be
+#'             returned in a warning message.
--- man-roxygen/wobble.R
+++ man-roxygen/wobble.R
@@ -2,7 +2,3 @@
 #'               proceeding with the test, in order to make sure there are no duplicate values in the
 #'               sample that can cause the scale estimate to be 0. Only necessary for the permutation and
 #'               randomization version of the test. The default is \code{wobble = FALSE}.
-#' @param wobble.seed integer value used as a seed for the random number generation in case of \code{wobble = TRUE}.
-#'                    If no seed is given, it is set randomly. In any case, the
-#'                    seed used will be returned in a warning message. The argument is
-#'                    ignored if wobble = FALSE.
