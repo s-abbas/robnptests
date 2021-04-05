716d7d116f07ce0aacf7f4956995254c1398536e
s-abbas <sermad.abbas@tu-dortmund.de>
2021-04-05 19:19:03 +0200

    test
    Added tests for location and scale equivariance in 'test_location_estimators.R'.

--- tests/testthat/test_location_estimators.R
+++ tests/testthat/test_location_estimators.R
@@ -25,9 +25,12 @@ testthat::test_that("trimmed_mean works correctly", {
                              mean(x = x, trim = 0.5)
   )
 
-  ## If all elements of 'x' are NA, the output needs to be NA
-  testthat::expect_identical(trim_mean(x = NA_real_, gamma = 0.2, na.rm = TRUE), NA_real_)
-  testthat::expect_identical(trim_mean(x = NA_real_, gamma = 0.2, na.rm = FALSE), NA_real_)
+  ## The estimator should be location and scale equivariant
+  testthat::expect_equal(trim_mean(x = 3 * x + 2), 3 * trim_mean(x = x) + 2)
+
+  ## The output should be a numeric scalar
+  checkmate::expect_numeric(trim_mean(x = x), len = 1)
+  checkmate::expect_numeric(trim_mean(x = c(x, NA), na.rm = FALSE), len = 1)
 })
 
 ## Winsorized mean ----
@@ -50,9 +53,12 @@ testthat::test_that("win_mean works correctly", {
   testthat::expect_equal(win_mean(x = x, gamma = 0.05), 55.65)
   testthat::expect_equal(win_mean(x = x, gamma = 0), mean(x = x))
 
-  ## If all elements of 'x' are NA, the output needs to be NA
-  testthat::expect_identical(win_mean(x = NA_real_, gamma = 0.2, na.rm = TRUE), NA_real_)
-  testthat::expect_identical(win_mean(x = NA_real_, gamma = 0.2, na.rm = FALSE), NA_real_)
+  ## The estimator should be location and scale equivariant
+  testthat::expect_equal(win_mean(x = 3 * x + 2), 3 * win_mean(x = x) + 2)
+
+  ## The output should be a numeric scalar
+  checkmate::expect_numeric(win_mean(x = x), len = 1)
+  checkmate::expect_numeric(win_mean(x = c(x, NA), na.rm = FALSE), len = 1)
 })
 
 ## One-sample Hodges-Lehmann estimator ----
@@ -72,9 +78,12 @@ testthat::test_that("hodges_lehmann works correctly", {
   ## The computed value has to be identical to 7
   testthat::expect_equal(hodges_lehmann(x), 7)
 
-  ## If all elements of 'x' are NA, the output needs to be NA
-  testthat::expect_identical(hodges_lehmann(x = rep(NA_real_, 2, na.rm = TRUE)), NA_real_)
-  testthat::expect_identical(hodges_lehmann(x = rep(NA_real_, 2, na.rm = FALSE)), NA_real_)
+  ## The estimator should be location and scale equivariant
+  testthat::expect_equal(hodges_lehmann(x = 3 * x + 2), 3 * hodges_lehmann(x = x) + 2)
+
+  ## The output should be a numeric scalar
+  checkmate::expect_numeric(hodges_lehmann(x = x), len = 1)
+  checkmate::expect_numeric(hodges_lehmann(x = c(x, NA), na.rm = FALSE), len = 1)
 })
 
 ## Two-sample Hodges-Lehmann estimator ----
@@ -110,10 +119,24 @@ testthat::test_that("hodges_lehmann_2sample works correctly", {
   x <- x - hodges_lehmann_2sample(x, y)
 
   testthat::expect_equal(sum(rank(c(x, y))[1:m]), m * (m + n + 1)/2)
+
+  ## Estimator should be location invariant and scale equivariant
+  testthat::expect_equal(hodges_lehmann_2sample(x = x + 2, y = y + 2),
+                         hodges_lehmann_2sample(x = x, y = y))
+  testthat::expect_equal(hodges_lehmann_2sample(x = 3 * x, y = 3 * y),
+                         3 * hodges_lehmann_2sample(x = x, y = y))
+
+  ## The output should be a numeric scalar
+  checkmate::expect_numeric(hodges_lehmann_2sample(x = x, y = y), len = 1)
+  checkmate::expect_numeric(hodges_lehmann_2sample(x = c(x, NA), y = y, na.rm = FALSE), len = 1)
 })
 
 ## M-estimator ----
 testthat::test_that("m_est works correctly", {
+
+  ## Generate exemplary input vector
+  set.seed(108)
+  x <- rnorm(10)
 
   ## Missing values should be removed correctly
   testthat::expect_equal(m_est(x = c(x, NA), psi = "huber", na.rm = TRUE),
@@ -126,6 +149,14 @@ testthat::test_that("m_est works correctly", {
                          m_est(x = c(x, NA), psi = "huber", na.rm = FALSE)
   )
 
-  ## TODO: Output needs to be tested.
-})
+  ## Location estimators should be location and scale equivariant
+  testthat::expect_equal(m_est(x = 3 * x + 2, psi = "huber")$est,
+                         3 * m_est(x = x, psi = "huber")$est + 2, tol = 10e-6)
 
+  ## Variance estimators should be location invariant and scale equivariant
+  testthat::expect_equal(m_est(x = 3 * x + 2, psi = "huber")$var,
+                         9 * m_est(x = x, psi = "huber")$var, tol = 10e-6)
+
+  ## The output should be a list contain two numeric values
+  checkmate::expect_list(m_est(x = x, psi = "huber"), types = rep("numeric", 2))
+})
