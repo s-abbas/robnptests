6989639bcd53f80ed95f8110fc3a3ad923ede64b
b-brune <bbrune@statistik.tu-dortmund.de>
2021-04-18 18:37:05 +0200

    documentation for automatic selection of the method

--- man-roxygen/method_m_test.R
+++ man-roxygen/method_m_test.R
@@ -2,4 +2,11 @@
 #'               for a permutation and \code{"randomization"} for a randomization test.
 #'               The permutation test uses all data splits into two samples,
 #'               while the randomization test draws \code{n.rep} random splits
-#'               with replacement.
+#'               with replacement. If not specified explicitly,
+#'               defaults to
+#'               \code{"permutation"},  if \code{m < 30}, \code{n < 30} and
+#'                \code{n.rep >= choose(m+n, m)},
+#'               \code{"randomization"}, if \code{m < 30}, \code{n < 30} and
+#'                \code{n.rep < choose(m+n, m)}, and
+#'                \code{"asymptotic"}, if \code{m >= 30} and \code{n >= 30}.
+
