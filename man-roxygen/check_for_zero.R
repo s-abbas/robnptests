d9090af1ee439b29a68826be6be0be0bfe68d824
s-abbas <sermad.abbas@tu-dortmund.de>
2021-04-18 19:51:48 +0200

    documentation
    Added roxygen template for argument 'check.for.zero'.

---
+++ man-roxygen/check_for_zero.R
@@ -0,0 +1,3 @@
+#' @param check.for.zero logical value indicating a warning should be triggered
+#'                       if the scale estimate is zero. The default is
+#'                       \code{FALSE}.
