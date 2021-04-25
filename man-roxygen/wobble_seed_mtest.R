cdb657fd74e0aa44b83358c7b175d4cee1bc0291
b-brune <bbrune@statistik.tu-dortmund.de>
2021-04-25 08:50:04 +0200

    precompiled vignette

---
+++ man-roxygen/wobble_seed_mtest.R
@@ -0,0 +1,6 @@
+#' @param wobble.seed integer value used as a seed for the random number
+#'                    generation in case that \code{var.test = TRUE}
+#'                    and one of the vectors \code{x} and
+#'                    \code{y} contains zeros. When no seed is specified, it is
+#'                    set randomly and printed in a warning The argument is ignored if
+#'                    \code{var.test = FALSE}.
