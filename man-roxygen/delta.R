90eb46e85df95082948e86c678de003e9ff10db4
s-abbas <sermad.abbas@tu-dortmund.de>
2021-05-31 17:59:21 +0200

    documentation
    Added information to template for documentation of argument 'delta' in 'delta.R'.

--- man-roxygen/delta.R
+++ man-roxygen/delta.R
@@ -1,3 +1,6 @@
-#' @param delta a numeric value indicating the true difference in the location or scale parameter, depending
-#'              on whether the test should be performed for a difference in location or in scale.
-#'              The default is \code{delta = 0} for location test and \code{delta = 1} for scale test.
+#' @param delta a numeric value indicating the true difference in the location or
+#'              scale parameter, depending on whether the test should be performed
+#'              for a difference in location or in scale. The default is
+#'              \code{delta = 0} for location test and \code{delta = 1} for scale
+#'              test. In case of \code{var.test = TRUE}, \code{delta} describes
+#'              the ratio squared scale parameters.
