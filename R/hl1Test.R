a9700055bf6a9b3b7319f23dfe7ae0f3fb3c2094
s-abbas <sermad.abbas@tu-dortmund.de>
2021-06-13 22:28:37 +0200

    documentation
    Revised comments/documentation in 'hl1Test.R'.

--- R/hl1Test.R
+++ R/hl1Test.R
@@ -22,50 +22,57 @@
 #'
 #' @details
 #' The test statistic for this test is based on the difference of the
-#' one-sample Hodges-Lehmann estimators of \code{x} and \code{y}.
-#' We offer three versions of the test: randomization, permutation and asymptotic.
+#' one-sample Hodges-Lehmann estimators of \code{x} and \code{y}, see
+#' \code{\link[robnptests]{hodges_lehmann}}. Three versions
+#' of the test are implemented: randomization, permutation, and asymptotic.
 #'
 #' The test statistic for the permutation and randomization version of the test
-#' is standardized using a robust scale estimator.
+#' is standardized using a robust scale estimator, see
+#' \insertCite{FriDeh11robu}{robnptests}.
 #'
-#' Setting \code{scale = "S1"} represents use of
+#' With \code{scale = "S1"}, the scale is estimated by
 #'
-#' \deqn{S = med(|X_i - X_j|: 1 \le i < j \le m, |Y_i - Y_j|, 1 \le i < j \le n),}
+#' \deqn{S = med(|x_i - x_j|: 1 \le i < j \le m, |y_i - y_j|, 1 \le i < j \le n),}
 #'
 #' whereas \code{scale = "S2"} uses
 #'
-#' \deqn{S = med(|Z_i - Z_j|: 1 \le i < j \le m+n).}
+#' \deqn{S = med(|z_i - z_j|: 1 \le i < j \le m + n).}
 #'
-#' Here, \eqn{ Z = ( X_1 - med(X),...,X_m - med(X), Y_1 - med(Y),...,Y_n - med(Y) )'}
-#' is the median-corrected sample. For more details see \insertCite{FriDeh11robu;textual}{robnptests}.
+#' Here, \eqn{z = (z_1, ..., z_{m + n}) = (x_1 - med(x), ..., x_m - med(x), y_1 - med(y), ..., y_n - med(y))}
+#' is the median-corrected sample.
 #'
-#' When computing the randomization distribution based on randomly drawn splits with
-#' replacement, the function \code{\link[statmod]{permp}} \insertCite{PhiSmy10perm}{robnptests}
+#' The randomization distribution is based on randomly drawn splits with
+#' replacement. The function \code{\link[statmod]{permp}} \insertCite{PhiSmy10perm}{robnptests}
 #' is used to calculate the p-value. For the asymptotic test, a transformed version
-#' of the the difference of the HL1 estimators is compared to the standard normal distribution.
-#' For more details see \insertCite{FriDeh11robu;textual}{robnptests}.
+#' of the difference of the HL1-estimators, which asymptotically follows a
+#' normal distribution, is used. For more details on the asymptotic test, see
+#' \insertCite{FriDeh11robu;textual}{robnptests}.
 #'
-#' For \code{var.test = TRUE}, the test compares the two samples for a difference in scale.
-#' This is achieved by log-transforming the original observations so that a potential
-#' scale difference appears as a location difference between the transformed samples;
-#' see \insertCite{Fri12onli;textual}{robnptests}. The sample should not contain zeros
-#' to prevent problems with the necessary log-transformation. If it contains zeros,
-#' uniform noise is added to all variables in order to remove zeros. A warning is
-#' printed.
+#' For \code{var.test = TRUE}, the test compares the two samples for a difference
+#' in scale. This is achieved by log-transforming the original squared observations,
+#' i.e. \code{x} is replaced by \code{log(x^2)} and \code{y} by \code{log(y^2)}.
+#' A potential scale difference then appears as a location difference between
+#' the transformed samples, see \insertCite{Fri12onli;textual}{robnptests}.
+#' The sample should not contain zeros to prevent problems with the necessary
+#' log-transformation. If it contains zeros, uniform noise is added to all
+#' variables in order to remove zeros and warning is printed.
 #'
-#' If the sample has been modified (either because of zeros for \code{var.test = TRUE}, or
-#' \code{wobble = TRUE}), the modified samples can be retrieved using
+#' If the sample has been modified (either because of zeros if \code{var.test = TRUE}
+#' or \code{wobble = TRUE}), the modified samples can be retrieved using
 #'
-#' \code{set.seed(wobble.seed); wobble(x, y)}
+#' \code{set.seed(wobble.seed); wobble(x, y)}.
 #'
 #' Both samples need to contain at least 5 non-missing values.
 #'
 #' @return
-#' A list with class "\code{htest}" containing the following components:
+#' A named list with class "\code{htest}" containing the following components:
 #' \item{statistic}{the value of the test statistic.}
 #' \item{p.value}{the p-value for the test.}
-#' \item{estimate}{the one-sample Hodges-Lehmann estimates of \code{x} and \code{y}.}
-#' \item{null.value}{the specified hypothesized value of the mean difference.}
+#' \item{estimate}{the one-sample Hodges-Lehmann estimates of \code{x} and \code{y}
+#'                 (if \code{var.test = FALSE}) or of \code{log(x^2)} and
+#'                 \code{log(y^2)} (if \code{var.test = TRUE}).}
+#' \item{null.value}{the specified hypothesized value of the mean difference/squared
+#'                   scale ratio.}
 #' \item{alternative}{a character string describing the alternative hypothesis.}
 #' \item{method}{a character string indicating how the p-value was computed.}
 #' \item{data.name}{a character string giving the names of the data.}
@@ -80,16 +87,16 @@
 #' \insertRef{Fri12onli}{robnptests}
 #'
 #' @examples
-#' ## Generate random samples
+#' # Generate random samples
 #' set.seed(108)
 #' x <- rnorm(20); y <- rnorm(20)
 #'
-#' ## Asymptotic HL1 test
+#' # Asymptotic HL1 test
 #' hl1_test(x, y, method = "asymptotic", scale = "S1")
 #'
 #' \dontrun{
-#' ## HL12 test using randomization principle by drawing 1000 random permutations
-#' ## with replacement
+#' # HL12 test using randomization principle by drawing 1000 random permutations
+#' # with replacement
 #'
 #' hl1_test(x, y, method = "randomization", n.rep = 1000, scale = "S2")
 #' }
@@ -108,7 +115,7 @@ hl1_test <- function(x,
                      wobble = FALSE,
                      wobble.seed = NULL) {
 
-  ## Check input arguments ----
+  # Check input arguments ----
   check_test_input(x = x, y = y, alternative = alternative, delta = delta,
                    method = method, scale = scale, n.rep = n.rep, na.rm = na.rm,
                    var.test = var.test, wobble = wobble, wobble.seed = wobble.seed,
